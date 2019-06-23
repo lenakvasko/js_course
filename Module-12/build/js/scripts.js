f58b2af04104d7ed9c19f23acf55242fa52ac40e
Lena Kvasko <kvaskoelena@gmail.com>
2019-06-23 21:43:16 +0300

    adds right version hw-12

--- Module-12/build/js/scripts.js
+++ Module-12/build/js/scripts.js
@@ -1,134 +1,156 @@
-'use strict';
-/* 
-  Напишите приложение для хранения url веб-страниц в виде карточек-закладок. 
-  
+/* eslint-disable eqeqeq */
+/* eslint-disable consistent-return */
+/* eslint-disable func-names */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-return-assign */
+/* eslint-disable no-use-before-define */
+/* eslint-disable max-len */
+/* eslint-disable no-undef */
+
+
+/*
+  Напишите приложение для хранения url веб-страниц в виде карточек-закладок.
+
   Реализуйте следующий функционал:
     - Используйте Gulp для сборки проекта, JS обработан транспайлером Babel, ресурсы оптимизированы
-    
+
     - Для добавления новой закладки, в приложении есть форма с элементом input и кнопкой "Добавить"
-    
+
     - В приложении есть список всех добавленных карточек-закладок, располагающийся под формой
-    
+
     - Некоторые элементы интерфейса создаются динамически. Используйте шаблонизатор Handlebars для
       создания списка карточек. Форма уже есть в HTML при загрузке страницы.
-      
+
     - При добавлении ссылки в поле формы и нажатии на кнопку "Добавить", происходят проверки:
         * на существование закладки с такой ссылкой в текущей коллекции закладок. Если такая закладка есть,
           всплывает диалоговое окно оповещающее пользователя о том, что такая закладка уже есть.
         * при условии валидной, еще не существующей в коллекции ссылки, карточка с такой ссылкой
           добавляется в коллекцию.
-          
+
     - В интерфейсе, новые карточки добавляются наверх списка, а не вниз.
-    
-    - Каждая карточка-закладка содержит кнопку для удаления карточки из коллекции, при клике 
+
+    - Каждая карточка-закладка содержит кнопку для удаления карточки из коллекции, при клике
       на кнопку происходит удаление.
-      
+
     - При повторном посещении страницы с одного и того же устройства и браузера, пользователь видит
       все карточки-закладки которые были во время последнего его посещения. Используйте localStorage
-      
+
   🔔 Оформление интерфейса произвольное
 */
 
 /*
   ⚠️ ЗАДАНИЕ ПОВЫШЕННОЙ СЛОЖНОСТИ - ВЫПОЛНЯТЬ ПО ЖЕЛАНИЮ
-  
-    - При добавлении ссылки в поле формы и нажатии на кнопку "Добавить", происходи проверка 
-      на валидность введенной ссылки: если был введен невалидный url то должно всплывать 
+
+    - При добавлении ссылки в поле формы и нажатии на кнопку "Добавить", происходи проверка
+      на валидность введенной ссылки: если был введен невалидный url то должно всплывать
       диалоговое окно, оповещающее пользователя о том, что это невалидный url. Используйте
       регулярные выражения для валидации url.
-          
     - Каждая карточка содержит превью изображение и базовую информацию о странице по адресу закладки,
       для получения этой информации воспользуйтесь этим Rest API - https://www.linkpreview.net/
 */
+const tryLocalStorage = (function (w) {
+  if (!w) return;
+  const isActive = 'localStorage' in w; // eslint-disable-next-line consistent-return
 
-var list = document.querySelector('.list');
-var form = document.querySelector('.js-form');
-var input = document.querySelector('.js-input');
-var tittle = document.querySelector('.hide');
-var source = document.querySelector('.laptops-tpl').innerHTML.trim();
-var template = Handlebars.compile(source);
-var urls = [];
+  const get = function get(key) {
+    try {
+      const serializedState = localStorage.getItem(key);
+      return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (err) {
+      console.error('Get state error: ', err);
+    }
+  };
 
-function allUrls(mas) {
-  var markup = mas.reduce(function (acc, el) {
-    return acc += template(el);
-  }, '');
-  list.innerHTML = markup;
-} //get all urls in one massive and build an html
+  const set = function set(key, value) {
+    try {
+      const serializedState = JSON.stringify(value);
+      localStorage.setItem(key, serializedState);
+    } catch (err) {
+      console.error('Set state error: ', err);
+    }
+  };
 
+  const publicAPI = {
+    isActive,
+    get,
+    set,
+  }; // eslint-disable-next-line consistent-return
 
-function checkValidUrl(url) {
-  var pattern = /^(ftp|http|https):\/\/[^ "]+$/;
-  var result = pattern.test(url);
-  var isWritted = urls.some(function (el) {
-    return el.url === url;
-  });
+  return publicAPI;
+}(window));
 
-  if (result && !isWritted) {
-    fetch("https://api.linkpreview.net/?key=5cd71a29bf3526610f7d14eb637f134f14713b1c85043&q=".concat(url)).then(function (resp) {
-      if (resp.ok) return resp.json();
-      throw new Error('Error' + resp.statusText);
-    }).then(function (data) {
-      urls.unshift({
-        url: data.url,
-        img: data.image
-      }), allUrls(urls);
-      set();
-    }).catch(function (err) {
-      return console.log('Error: ' + err);
-    });
-  } else if (isWritted) {
-    return alert("Such an url is existed!");
+let cards = [{
+  url: 'demo',
+  'logo-url': 'https://www.freelogodesign.org/Content/img/logo-ex-7.png',
+}];
+
+if (tryLocalStorage.get('cardsData')) {
+  cards = tryLocalStorage.get('cardsData');
+}
+
+const elements = {
+  form: document.querySelector('.js-form'),
+  input: document.querySelector('input'),
+  result: document.querySelector('.result-list'),
+  source: document.querySelector('#template-container').innerHTML.trim(),
+};
+let inputValue; // eslint-disable-next-line no-undef
+
+const template = Handlebars.compile(elements.source); // eslint-disable-next-line no-use-before-define
+
+cardsViewer();
+elements.form.addEventListener('submit', formSubmitClick);
+
+function formSubmitClick(evt) {
+  inputValue = elements.input.value;
+  elements.input.value = '';
+  evt.preventDefault();
+  addNewItem();
+}
+
+function addNewItem() {
+  // eslint-disable-next-line no-useless-escape
+  const checkURL = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+
+  if (cards.some(elem => inputValue === elem.url)) {
+    // eslint-disable-next-line no-alert
+    alert('Такая закладка уже есть');
+  } else if (!checkURL.test(inputValue)) {
+    // eslint-disable-next-line no-alert
+    alert('не верный формат');
   } else {
-    return alert("It's unvalid url!");
-  }
-}
-
-function set() {
-  localStorage.setItem("fetchTags", JSON.stringify(urls));
-} //download from the workspace to localstorage
-
-
-function get() {
-  var data = localStorage.getItem("fetchTags");
-  var mas = JSON.parse(data);
-
-  if (mas !== null) {
-    return mas;
-  }
-
-  ;
-} //take elements from the localstorage
-
-
-function updateLocMas() {
-  if (localStorage.getItem('fetchTags')) {
-    var mas = JSON.parse(localStorage.getItem('fetchTags'));
-    urls = mas;
-  }
-}
-
-function handleDel(_ref) {
-  var target = _ref.target;
-
-  if (target.nodeName === 'BUTTON') {
-    console.log(urls);
-    urls = urls.filter(function (el) {
-      return target.previousElementSibling.innerHTML != el.url;
+    fetchGetImage(inputValue).then((res) => {
+      cards.push({
+        url: inputValue,
+        'logo-url': res,
+      // eslint-disable-next-line no-sequences
+      }), cardsViewer();
+      tryLocalStorage.set('cardsData', cards);
     });
-    allUrls(urls);
-    set();
   }
 }
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  console.log(input.value);
-  checkValidUrl(input.value);
-  allUrls(urls);
-  set();
-});
-list.addEventListener('click', handleDel);
-updateLocMas();
-allUrls(get());
-console.log(urls);
+function fetchGetImage(url) {
+  const data = {
+    key: '5cd40f3bec0e35c20a74cf61b9b1d52cacfe039b93914',
+    q: url,
+  };
+  return fetch('https://api.linkpreview.net/?key='.concat(data.key, '&q=').concat(data.q)).then(res => res.json()).then(response => response.image).catch(e => console.error(e));
+}
+
+elements.result.addEventListener('click', deleteResultClick);
+
+function deleteResultClick(evt) {
+  if (evt.target.nodeName === 'BUTTON') {
+    cards = cards.filter(elem => evt.target.parentNode.firstChild.innerHTML != elem.url);
+    cardsViewer();
+    tryLocalStorage.set('cardsData', cards); // eslint-disable-next-line no-empty
+  } else {}
+}
+
+function cardsViewer() {
+  // eslint-disable-next-line no-return-assign
+  // eslint-disable-next-line no-param-reassign
+  const markup = cards.reduce((acc, el) => acc += template(el), '');
+  elements.result.innerHTML = markup;
+}
